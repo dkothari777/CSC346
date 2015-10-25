7b5528ad0c4520d0109a98a99761a20657d7972d
Shawn Kothari <dkothari@email.arizona.edu>
2015-10-25 16:13:59 -0700

    I thinked I finished the monitoring tier

---
+++ Assignment4/delta.py
@@ -0,0 +1,9 @@
+from pprint import pprint
+from hashlib import sha256
+import hmac
+import dropbox
+import ConfigParser
+
+def process_delta(*args):
+    print "Processing Delta!"
+    return
--- Assignment4/monitor.py
+++ Assignment4/monitor.py
@@ -1,5 +1,8 @@
 from flask import Flask
-from ppringt import pprint
+from pprint import pprint
+from hashlib import sha256
+import hmac
+import threading
 import dropbox
 import ConfigParser
 app = Flask(__name__)
@@ -10,17 +13,29 @@ changes = []
 config = ConfigParser.RawConfigParser()
 config.read("settings.cfg")
 token = config.get('Dropbox', 'token')
+APP_SECRET = config.get('Dropbox', 'secret')
 client = dropbox.client.DropboxClient(token)
-
 @app.route("/webhook", methods=['GET'])
 def verify():
-    '''Respon to the webhook verficaton (GET request) by echoing back the challenge parameter.'''
+    '''Response to the webhook verficaton (GET request) by echoing back the challenge parameter.'''
     return request.args.get('challenge')
 
-@app.errorhandler(404)
+@app.route("/webhook", methods=['POST'])
+def webhook():
+    '''Receive a list of changed user IDs from Dropbox and process each.'''
+    signature = request.headers.get('X-Dropbox-Signature')
+    if signature != hmac.new(APP_SECRET, request.data, sha256).hexdigest():
+        abort(403)
+
+    for d in json.loads(request.data)['delta']:
+        #do stuff
+        threading.Thread(target=process_delta, args=(d,)).start()
+    return
+
+@app.errorhandler(403)
 def page_not_found(e):
-    """Return a custom 404 error. """
-    return 'Sorry, nothing at this URL', 404
+    """Return a custom 403 error. """
+    return 'Sorry, secret does not match signature', 403
 
 if __name__ == "__main__":
     app.run()
